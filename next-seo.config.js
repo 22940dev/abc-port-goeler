27b64b74c317b4b9e33da479cb7cac71a975d790
Patrick <patrickvongoeler@gmail.com>
2021-01-08 13:14:45 +0100

    Fix SEO banner

--- next-seo.config.js
+++ next-seo.config.js
@@ -4,16 +4,16 @@ const description = "Full-Stack Web Developer, Explorer of new Technologies, Ama
 const SEO = {
     title,
     description,
-    canonical: "https://leerob.io",
+    canonical: "https://pgvr.dev",
     openGraph: {
         type: "website",
         locale: "en_IE",
-        url: "https://leerob.io",
+        url: "https://pgvr.dev",
         title,
         description,
         images: [
             {
-                url: "https://leerob.io/static/images/banner.jpg",
+                url: "https://pgvr.dev/static/images/banner.png",
                 alt: title,
                 width: 1280,
                 height: 720,
